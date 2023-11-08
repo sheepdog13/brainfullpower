e3b4959c4d12cb4801a39f6db2b1504e597c4e5a
sheepdog13 <mikecho1234@naver.com>
2023-11-09 02:16:43 +0900

    problem page css

--- src/pages/Problem.tsx
+++ src/pages/Problem.tsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import SvgIcon from "@mui/material/SvgIcon";
+import TimerIcon from "@mui/icons-material/TimerOutlined";
 
 const Wrapper = styled.div`
   width: 100%;
@@ -9,17 +11,146 @@ const Wrapper = styled.div`
 `;
 
 const TimeBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding-left: 8px;
   width: 94px;
   height: 32px;
   margin-top: 60px;
   border-radius: 999px;
   background-color: #495057;
+  color: #eaedf0;
+`;
+
+const TimerNum = styled.span`
+  margin-left: 4px;
+`;
+
+const LevelNav = styled.div`
+  width: 335px;
+  height: 40px;
+  margin-top: 38px;
+  border-top-right-radius: 12px;
+  border-top-left-radius: 12px;
+  background-color: #4757ff;
+  border: 2px solid #212529;
+`;
+
+const LevelToggles = styled.ul`
+  display: flex;
+  margin-left: 12px;
+  margin-top: 13px;
+`;
+
+const LevelToggle = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 56px;
+  height: 26px;
+  background-color: #c1c6cc;
+  border-radius: 12px 12px 0 0;
+  border-width: 2px;
+  border-style: solid;
+  border-color: #212529;
+  color: #495057;
+  font-size: 14px;
+  margin-left: -2px;
+  &:first-child {
+    background-color: #fff;
+    border-bottom: none;
+    font-weight: bold;
+    color: #4757ff;
+  }
+`;
+
+const ContentBox = styled.div`
+  position: relative;
+  width: 335px;
+  height: 340px;
+  margin-bottom: 38px;
+  background-color: #fff;
+  border: 2px solid #212529;
+  border-top: none;
+  border-radius: 0 0 12px 12px;
+  div {
+    position: absolute;
+    top: 8px;
+    left: -2px;
+    z-index: -1;
+    border-radius: 12px;
+    width: 335px;
+    height: 340px;
+    background-color: #212529;
+  }
+`;
+
+const BtnBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 335px;
+  gap: 16px;
+  margin-top: 16px;
+`;
+
+const Btn = styled.button`
+  position: relative;
+  width: 159.5px;
+  height: 90px;
+  border-radius: 12px;
+  background-color: #fff;
+  border: solid 2px #212529;
+  color: #212529;
+  font-size: 32px;
+  font-weight: bold;
+  div {
+    position: absolute;
+    top: 8px;
+    left: -2px;
+    z-index: -1;
+    border-radius: 12px;
+    width: 159.5px;
+    height: 90px;
+    background-color: #212529;
+  }
 `;
 
 function Problem() {
   return (
     <Wrapper>
-      <TimeBox></TimeBox>
+      <TimeBox>
+        <SvgIcon component={TimerIcon} sx={{ fontSize: 20 }} />
+        <TimerNum>00:00</TimerNum>
+      </TimeBox>
+      <LevelNav>
+        <LevelToggles>
+          <LevelToggle>Lv.1</LevelToggle>
+          <LevelToggle>Lv.2</LevelToggle>
+          <LevelToggle>Lv.3</LevelToggle>
+          <LevelToggle>Lv.4</LevelToggle>
+          <LevelToggle>Lv.5</LevelToggle>
+        </LevelToggles>
+      </LevelNav>
+      <ContentBox>
+        <div></div>
+      </ContentBox>
+      <BtnBox>
+        <Btn>
+          1<div></div>
+        </Btn>
+        <Btn>
+          2<div></div>
+        </Btn>
+      </BtnBox>
+      <BtnBox>
+        <Btn>
+          3<div></div>
+        </Btn>
+        <Btn>
+          4<div></div>
+        </Btn>
+      </BtnBox>
     </Wrapper>
   );
 }
