bcba164e7d6de4d7b0a16c71197218715c53eaa6
sheepdog13 <mikecho1234@naver.com>
2023-11-12 00:10:17 +0900

    setupproxy 설정 삭제

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,11 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+//const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = function (app) {
-  app.use(
-    "/v1",
-    createProxyMiddleware({
-      target: `${process.env.REACT_APP_API_URL}`,
-      changeOrigin: true,
-    })
-  );
-};
+// module.exports = function (app) {
+//   app.use(
+//     "/v1",
+//     createProxyMiddleware({
+//       target: `${process.env.REACT_APP_API_URL}`,
+//       changeOrigin: true,
+//     })
+//   );
+// };
